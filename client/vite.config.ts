10579cb40c480553cf76e1ba6c37e3aecf3d94ba
addy <addayounes8@gmail.com>
2023-02-01 18:59:18 +0100

    added .editorconfig file

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,10 +4,10 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-        },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
+  },
 });
