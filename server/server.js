2c4cc7ebe236eb0fc8df712bd6c52fff2e973cc6
dinoelhadj <dino.elhadj@gmail.com>
2023-02-06 19:59:38 +0100

    file upload

--- server/server.js
+++ server/server.js
@@ -1,1 +1,20 @@
-// TODO: create the express server and configure routers
+const express = require('express')
+const fileUpload = require('express-fileupload')
+
+const PORT = process.env.PORT || 3500
+
+const app = express()
+
+//limit file upload size, BusBoy documentation https://github.com/mscdex/busboy#api
+app.use(fileUpload({
+    limits: { fileSize: 50 * 1024 * 1024 },
+}));
+
+app.post('/upload', function (req, res) {
+    console.log(req.files.foo); // the uploaded file object
+});
+
+
+app.listen(PORT, () =>
+    console.log(`Server running on port ${PORT}`)
+)
