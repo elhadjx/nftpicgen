7ece47f5c0aec4a02cc6c34c13f3ecd9210d932f
elhadjx <dino.elhaj@gmail.com>
2023-02-07 12:15:39 +0100

    Single file

--- server/server.js
+++ server/server.js
@@ -10,10 +10,28 @@ app.use(fileUpload({
     limits: { fileSize: 100 * 1024 * 1024 },
 }));
 
-app.post('/upload', function (req, res) {
-    console.log(req.files.foo); // the uploaded file object
-});
 
+app.post('/upload', function (req, res) {
+    let sampleFile;
+    let uploadPath;
+
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
+
+    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    sampleFile = req.files.sampleFile;
+    //uploadPath = __dirname + '/somewhere/on/your/server/' + sampleFile.name;
+
+    // Use the mv() method to place the file somewhere on your server
+    sampleFile.mv(uploadPath, function (err) {
+        if (err)
+            return res.status(500).send(err);
+
+        res.send('File uploaded!');
+        console.log('File Uploaded!')
+    });
+});
 
 app.listen(PORT, () =>
     console.log(`Server running on port ${PORT}`)
