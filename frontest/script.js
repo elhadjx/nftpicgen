b139d7eae9faab62bd701b9e531059c9a1c62f85
elhadjx <dino.elhaj@gmail.com>
2023-02-12 10:26:04 +0100

    design

--- frontest/script.js
+++ frontest/script.js
@@ -4,6 +4,11 @@ console.log(gg)
 
 document.getElementById("uploadForm").style.display = "none";
 
+function uploadFiles() {
+    const input = document.querySelector('input[type="file"]');
+    console.log(input.files[0])
+}
+
 function showUploadForm() {
     document.getElementById("uploadForm").style.display = "block";
     document.getElementById("landingDiv").style.display = "none";
@@ -18,14 +23,16 @@ function generateToken() {
 }
 
 function addLayer() {
+    uploadFiles()
     const layers = document.getElementById("uploadForm")
     layers.removeChild(document.getElementById('submitButton'))
     layers.removeChild(document.getElementById('buttonAdd'))
     let layer_index = layers.childElementCount - 1;
     layers.innerHTML += `
                 <div class="fileLayer" id="layer${layer_index}">
-                    <input type="file" text="${layer_index}" name="files_layer_${layer_index}" accept="image/png" multiple />
-                </div>
+                    <h3>Layer ${layer_index}</h3>
+                    <input type="file" class="custom-file-input" name="files_layer_${layer_index}" accept="image/png" multiple />
+                    </div>
                 <input class="btn" type='submit' id="submitButton" value='Upload' />
                 <button class="btn" id="buttonAdd" onclick="addLayer()">Add Layer</button>
             `
