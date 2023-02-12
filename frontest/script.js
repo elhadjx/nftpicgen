049fbb89ec7f512c9371e73912acd2272503512a
elhadjx <dino.elhaj@gmail.com>
2023-02-12 10:44:42 +0100

    clear js

--- frontest/script.js
+++ frontest/script.js
@@ -4,13 +4,6 @@ console.log(gg)
 
 document.getElementById("uploadForm").style.display = "none";
 
-
-const tween = KUTE.fromTo(
-    '#blob1',
-    { path: '#blob1' },
-    { path: '#blob2' },
-    { repeat: 999, duration: 3000, yoyo: true }
-).start();
 function uploadFiles() {
     const input = document.querySelector('input[type="file"]');
     console.log(input.files[0])
